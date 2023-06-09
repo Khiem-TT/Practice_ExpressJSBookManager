76734806af3cc18d59d042e797d2b6efb7897725
tranthekhiem14112001 <trankhiem99999@gmail.com>
2023-06-09 10:37:29 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 8000;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'book_manager',
+    charset: 'utf8_general_ci'
+});
+
+connection.connect(err => {
+    if (err) {
+        throw err.stack;
+    } else {
+        console.log('connect database successfully');
+    }
+});
+
+app.get('/books/create', (req, res) => {
+    res.render('create');
+});
+
+app.post('/books/create', (req, res) => {
+    const {name, price, status, author} = req.body;
+    console.log(req.body);
+    const sqlInsert = "INSERT INTO books (name, price, status, author) VALUES ?";
+    const value = [
+        [name, price, status, author]
+    ];
+    connection.query(sqlInsert, [value], (err, result) => {
+        if (err) throw err;
+        res.end('success');
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
