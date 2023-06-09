afd4afbd1b562b0ce428877ce00d541962310110
tranthekhiem14112001 <trankhiem99999@gmail.com>
2023-06-09 10:56:53 +0700

    second commit

--- index.js
+++ index.js
@@ -42,6 +42,14 @@ app.post('/books/create', (req, res) => {
     });
 });
 
+app.get('/index', (req, res) => {
+    const sql = 'SELECT * FROM books';
+    connection.query(sql, (err, result) => {
+        if (err) throw err;
+        res.render('index', {data: result});
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
